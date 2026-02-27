218f8d9c08e51b0d8f1403e8975da17f997bacfd
marceline <marcelinelentz@gmail.com>
2026-02-27 13:40:32 +1000

    Creating Book Database
    - create scriptable objects to store book data
    - flag enum for genre
    - creating UI area to display books, grid organisation functionality & scrollbar/scroll ability
    
    needs/pending fix: scroll area not set correctly & keeps snapping back/scroll bar indicating incorrect scroll area, selecting some genre enum tabs will randomly apply a random range of tags

---
+++ Assets/Scripts/BookData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BookData", menuName = "Scriptable Objects/BookData")]
+public class BookData : ScriptableObject
+{
+    public string bookTitle;
+    public string authorName;
+    public float publicationYear;
+    public bookGenre BookGenre;
+    public string bookBio;
+    public Sprite bookCover;
+
+}
+
+[Flags]
+public enum bookGenre { Horror, Romance, Childrens, Academic, Play, Tragedy, Gothic, Literary, Vampire, Fantasy, Epic, Crime, Mystery };
